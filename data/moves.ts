15596e5eea8682ebe63b363655b61eed5c3cf45b
Jeremy Roberts <jeremypeter@aol.com>
2021-05-22 00:41:39 -0700

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -2335,7 +2335,6 @@ export const Moves: {[moveid: string]: MoveData} = {
 			if (attacker.removeVolatile(move.id)) {
 				return;
 			}
-			this.add('-prepare', attacker, move.name);
 			if (!this.runEvent('ChargeMove', attacker, defender, move)) {
 				return;
 			}
@@ -2732,7 +2731,6 @@ export const Moves: {[moveid: string]: MoveData} = {
 			if (attacker.removeVolatile(move.id)) {
 				return;
 			}
-			this.add('-prepare', attacker, move.name);
 			if (!this.runEvent('ChargeMove', attacker, defender, move)) {
 				return;
 			}
