a125732af7be63541a454f4574b12f1cbc7156f2
Jeremy Roberts <jeremypeter@aol.com>
2021-05-23 03:36:17 -0700

    Update learnsets.ts

--- data/learnsets.ts
+++ data/learnsets.ts
@@ -666,6 +666,11 @@ export const Learnsets: {[speciesid: string]: LearnsetData} = {
 			cooldown: ["8L1"],
 			yellowgigahit: ["8L1"],
 			hit: ["8M"],
+			supercell: ["8L1"],
+			weatherfront: ["8L1"],
+			yellowsoak: ["8L1"],
+			yellowshift: ["8L1"],
+			yellowslap: ["8L1"],
 		},
 	},
 	yellowater: {
