29939c5b804b98b872dfaafe83f9e02bead49d26
Jeremy Roberts <jeremypeter@aol.com>
2021-08-16 20:42:55 -0700

    Update formats-data.ts

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -561,6 +561,10 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 		isNonstandard: "Thing",
 		tier: "Thing",
 	},
+	mistimink: {
+		isNonstandard: "Thing",
+		tier: "Thing",
+	},
 
 	// BASE GAME
 	bulbasaur: {
