4de28f1ce04c9a7b6cfb3f8611a0c272ec254bde
Jeremy Roberts <jeremypeter@aol.com>
2021-08-18 19:43:14 -0700

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -1584,7 +1584,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		priority: 0,
 		flags: {protect: 1, mirror: 1, defrost: 1},
 		onBasePower(basePower, pokemon) {
-			if (pokemon.hasType('Green') {
+			if (pokemon.hasType('Green')) {
 				return this.chainModify(2);
 			}
 		},
